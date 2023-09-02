2e1cab7ab1084ebcad22aeaf327846519ccb96c5
Motta <125099026+Mottaqbota@users.noreply.github.com>
2023-09-01 23:27:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+function getNoteValue() {
+  const inputNote = document.querySelector('.input-note').value;
+  const inputGrade = document.querySelector('.input-grade').value;
+  const cardNotes = document.querySelector('#card-newNotes');
+  if (isNaN(inputNote) || inputNote <= 0 || inputNote > 10) {
+    alert("Valor inválido. Por favor, insira uma nota entre zero e dez.");
+  } else {
+    alert("Nota salva com sucesso!")
+    cardNotes.innerHTML += `
+    <h1>Matéria: <strong> ${inputGrade} </strong></h1>
+    <p>Nota: <strong> ${inputNote} </strong></p>
+    `
+  }
+};
