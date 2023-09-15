f07f71d77e4210cc6854142ccec9054477cf11a8
Motta <125099026+Mottaqbota@users.noreply.github.com>
2023-09-15 15:17:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,113 @@
-function getNoteValue() {
-  const inputNote = document.querySelector('.input-note').value;
-  const inputGrade = document.querySelector('.input-grade').value;
-  const cardNotes = document.querySelector('#card-newNotes');
-  if (isNaN(inputNote) || inputNote <= 0 || inputNote > 10) {
-    alert("Valor inválido. Por favor, insira uma nota entre zero e dez.");
-  } else {
-    alert("Nota salva com sucesso!")
-    cardNotes.innerHTML += `
-    <h1>Matéria: <strong> ${inputGrade} </strong></h1>
-    <p>Nota: <strong> ${inputNote} </strong></p>
-    `
-  }
-};
+@import url('https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  font-family: 'Lexend';
+  box-sizing: border-box;
+  transition: all ease 0.2s;
+}
+
+button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  border: none;
+  outline: solid 2px #0071b3;
+  box-shadow: 0px 4px 0px 2px #0071b3;
+  padding: 1rem;
+  border-radius: 12px;
+  color: #012a41;
+  background: #00a2ff;
+}
+
+button:hover {
+  cursor: pointer;
+  filter: brightness(0.9);
+}
+
+button:active {
+  box-shadow: none;
+}
+button.--ativo {
+  filter: brightness(.8);
+  box-shadow: inset 0px 4px 0px 0px #0071b3;
+}
+
+.clear-button {
+  font-weight: 600;
+  color: #fff;
+  background: #ff0000;
+  outline: solid 2px #b30000;
+  box-shadow: 0px 4px 0px 2px #b30000;
+}
+
+.fa-solid {
+  font-size: 26px;
+}
+
+input {
+  border: none;
+  outline: solid 2px #0071b3;
+  padding: 1rem;
+  border-radius: 12px;
+  color: #012a41;
+  /* background: #00a2ff; */
+}
+
+input:focus {
+  outline: solid 2px #0071b3;
+  box-shadow: 0px 4px 0px 2px #0071b3;
+}
+
+main {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  gap: 1rem;
+  padding: 1rem;
+}
+
+section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  padding: 1rem;
+  gap: 1rem;
+  border-bottom: solid 2px #012a41;
+}
+
+.menu {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  padding: 1rem;
+}
 
 
-const act1 = document.getElementById('atividade1');
+.card-activities {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  padding: 1rem;
+  gap: 1rem;
+  width: 50%;
+  border-radius: 12px;
+  outline: solid 2px #012a41;
+  box-shadow: 0px 4px 0px 2px #012a41;
+}
 
-
-function startTimer() {
-  const timerDiv = document.getElementById('timer');
-  let timerCount = 10;
-  var cronometro = setInterval(() => {
-      timerDiv.innerHTML = `<p>${timerCount--}</p>`;
-    if (timerCount <= 0) {
-      clearInterval(cronometro)
-        timerDiv.innerHTML = `<p> Feliz Ano Novo!!!!🎊 🎉🎊 🎉🎊 🎉 </p>`;
-      }
-  }, 1000);
-};
-
-function enviar(){
-  let inputUser = document.getElementById('usuario-input');
-  let inputSenha = document.getElementById('senha-input');
-
-  if(inputUser.value === inputSenha.value) {
-    alert('Insira uma senha diferente do Usuário!')
-  } else {
-    alert('Senha Cadastrada com sucesso!')
-  }
+#atividade1,
+#atividade2,
+#atividade3,
+#atividade4 {
+  display: none;
 }
